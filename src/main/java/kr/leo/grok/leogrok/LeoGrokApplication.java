a970937e2c6632468971f793da099a48749a1eb7
doojin kim <doojin88@gmail.com>
2019-04-11 17:51:24 +0900

    create maven project

---
+++ src/main/java/kr/leo/grok/leogrok/LeoGrokApplication.java
@@ -0,0 +1,13 @@
+package kr.leo.grok.leogrok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeoGrokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeoGrokApplication.class, args);
+	}
+
+}
